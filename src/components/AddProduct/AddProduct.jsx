8f149cbe7f2fcd1d9e138829a66d17595e3c459d
Mahin-Hasan <mahinmtrs@gmail.com>
2023-12-05 07:17:53 +0600

    added add product form design and fixed submit error

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,9 +1,133 @@
+import formbg from '../../assets/formbg.png'
+
 
 const AddProduct = () => {
+    const handleAddCar = event => {
+
+        event.preventDefault();
+
+        const form = event.target;
+
+        const image = form.image.value;
+        const brandName = form.brandName.value;
+        const carName = form.carName.value;
+        const carType = form.carType.value;
+        const carPrice = form.carPrice.value;
+        const carRating = form.rating.value;
+        const carDescription = form.carDescription.value;
+
+        const newCar ={image,brandName,carName,carType,carPrice,carRating,carDescription};
+        console.log(newCar);
+
+
+    }
+
+
     return (
-        <div>
-            <h1>this is add product</h1>
-        </div>
+        <section className="container mx-auto">
+            <div className="text-center mb-6">
+                <h1 className="text-2xl my-6 sm:text-4xl  uppercase text-stone-500 font-medium leading-none sm:leading-tight">Add your brand</h1>
+            </div>
+            <div style={{ backgroundImage: `url(${formbg})` }} className="bg-cover bg-center rounded-none sm:rounded-lg my-16">
+                <div className="py-28">
+                    <h1 className='text-fuchsia-100 text-center text sm:text-3xl'>Provide all the necessary inforamtion regarding your car brand.</h1>
+                    <div className="bg-white border-4 border-fuchsia-800 p-14 max-w-4xl mx-auto">
+                        <form onSubmit={handleAddCar}>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
+                                <div>
+                                    <label className="label text-purple-700 text-lg font-medium">
+                                        Brand Image
+                                    </label>
+                                    <input
+                                        placeholder="Image URL"
+                                        type="text"
+                                        name="image"
+                                        className="form-control bg-[#fcf7ff] w-full border-2 py-3"
+                                    />
+                                </div>
+                                <div>
+                                    <label className="label text-purple-700 text-lg font-medium">
+                                        Brand Name
+                                    </label>
+                                    <input
+                                        placeholder="Brand Name"
+                                        type="text"
+                                        name="brandName"
+                                        className="form-control bg-[#fcf7ff] w-full border-2 py-3"
+                                    />
+                                </div>
+                            </div>
+                            {/* 2nd */}
+                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
+                                <div>
+                                    <label className="label text-purple-700 text-lg font-medium">
+                                        Car Name
+                                    </label>
+                                    <input
+                                        placeholder="Name"
+                                        type="text"
+                                        name="carName"
+                                        className="form-control bg-[#fcf7ff] w-full border-2 py-3"
+                                    />
+                                </div>
+                                <div>
+                                    <label className="label text-purple-700 text-lg font-medium">
+                                        Car Type
+                                    </label>
+                                    <input
+                                        placeholder="type"
+                                        type="text"
+                                        name="carType"
+                                        className="form-control bg-[#fcf7ff] w-full border-2 py-3"
+                                    />
+                                </div>
+                            </div>
+                            {/* 3rd */}
+                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
+                                <div>
+                                    <label className="label text-purple-700 text-lg font-medium">
+                                        Car Price
+                                    </label>
+                                    <input
+                                        placeholder="Price"
+                                        type="text"
+                                        name="carPrice"
+                                        className="form-control bg-[#fcf7ff] w-full border-2 py-3"
+                                    />
+                                </div>
+                                <div>
+                                    <label className="label text-purple-700 text-lg font-medium">
+                                        Product Rating
+                                    </label>
+                                    <div className="rating block mt-3">
+                                        <input type="radio" name="rating" value="1" className="mask mask-star-2 bg-orange-400" />
+                                        <input type="radio" name="rating" value="2" className="mask mask-star-2 bg-orange-400" />
+                                        <input type="radio" name="rating" value="3" className="mask mask-star-2 bg-orange-400" />
+                                        <input type="radio" name="rating" value="4" className="mask mask-star-2 bg-orange-400" />
+                                        <input type="radio" name="rating" value="5" className="mask mask-star-2 bg-orange-400" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="">
+                                <label className="label text-purple-700 text-lg font-medium">
+                                    Car Description
+                                </label>
+                                <textarea
+                                    placeholder="Enter detailed information of the car"
+                                    className="bg-[#fcf7ff] w-full border-2 py-4"
+                                    name="carDescription"
+                                />
+                            </div>
+                            <div className="form-control mt-6">
+                                {/* <button className="btn btn-neutral text-lg text-fuchsia-500">Add Car Details</button> */}
+                            </div>
+                            <input className="btn btn-neutral w-full" type="submit" value="Add Car Details" />
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,24 @@
 const Home = () => {
     return (
-        <div>
-            <h1>this is home</h1>
-        </div>
+        <main>
+            <section>
+                <h1>add banner</h1>
+            </section>
+            {/* add 6 categories */}
+            <section>
+                    <div className="text-center">
+                        <h1 className="text-6xl text-fuchsia-800">Are you in search for a new car?</h1>
+                        <p>Experience automotive excellence at its finest with JDM Automobiles. Our curated selection of cars combines cutting-edge technology and sleek design for an unparalleled driving experience. From fuel-efficient compacts to luxurious SUVs, we have the perfect vehicle for every lifestyle. Choose JDM Automobiles for quality, reliability, and exceptional service. Elevate your drive with us – where excellence meets the road.</p>
+                        <p>Look no further, We provide wide collection of japanese cars ranging from various brands.</p>
+                    </div>
+            </section>
+            {/* add 6 image layout */}
+
+            {/* add faq */}
+            <section>
+
+            </section>
+        </main>
     );
 };
 
